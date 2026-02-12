6e5cf98456c1833b8f30044bc4f3d6685731ea49
Lucas Lange <lucas@langes.us>
2026-02-12 11:58:07 -0500

    use localStorage name 'saved-page' to hold the last page set

--- src/AppContext.tsx
+++ src/AppContext.tsx
@@ -194,7 +194,10 @@ export function AppProvider({ children }: { children: ReactNode }) {
         currentUser,
         setCurrentUser,
         currentPage,
-        setCurrentPage,
+        setCurrentPage: (page) => { 
+          localStorage.setItem('saved-page', page);
+          setCurrentPage(page);
+        },
         libraryFiles,
         setLibraryFiles,
         assessments,
