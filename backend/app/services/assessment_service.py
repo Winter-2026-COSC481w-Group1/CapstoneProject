20b5a846cf8b97f1cbc671f8fc54017ed0460fb2
pkmartus <pkmartus@gmail.com>
2026-02-13 08:47:24 -0500

    save changes to merge with feat branch

--- backend/app/services/assessment_service.py
+++ backend/app/services/assessment_service.py
@@ -1,43 +1,53 @@
 import json
+import os
 from typing import List
 from app.schemas.assessment import AssessmentSchema
+import google.generativeai as genai
+from fastapi import HTTPException
 
 class AssessmentService:
+    def __init__(self):
+       genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+
+       self.model = genai.GenerativeModel(
+            model_name="gemini-1.5-flash",
+            #the model is pre-loaded to use json and our assessment scheme
+            generation_config={
+                "response_mime_type": "application/json",
+                "response_schema": AssessmentSchema
+            }
+        )
+
+    #todo find out the format of context chunks
     def generate_assessment(self, context_chunks: list, difficulty: str, 
                             types: List[str]) -> AssessmentSchema:
-        
-        #Get the schema format to send to the LLM
-        schema_format = AssessmentSchema.model_json_schema()
-        #Construct prompt with strict JSON formatting instructions
-        prompt = f"""
-        You are an exam writer. Based ONLY on the provided context, gernerate an assessment.
-        
-        CONTEXT: {chr(10).join(context_chunks)}
-
-        DIFFICULTY: {difficulty}
-
-        QUESTION TYPES: 
-        Questions should only be of the types specified in this list:
-        {', '.join(types)}
-
-        OUTPUT INSTRUCTIONS:
-        You must return ONLY a JSON object that matches this schema EXACTLY:
-        {schema_format}
-
-        Make sure that every question has a unique ID and accurately reflects the context.
-        """
-        
-        # 2. call LLM
-        raw_response = self.call_llm(prompt) # Placeholder for your Ollama logic
-        
         try:
-            # 3. SCHEMA ENFORCEMENT
-            # This will raise an error if LLM misses a field or hallucinate a type
+            #Construct prompt with strict JSON formatting instructions
+            prompt = f"""
+            You are an exam writer. Based ONLY on the provided context, gernerate an assessment.
+
+            CONTEXT: {chr(10).join(context_chunks)}
+
+            DIFFICULTY: {difficulty}
+
+            QUESTION TYPES: 
+            Questions should only be of the types specified in this list:
+            {', '.join(types)}
+
+            OUTPUT INSTRUCTIONS:
+            You must return ONLY a JSON object that matches the schema EXACTLY:
+
+            Make sure that every question has a unique ID and accurately reflects the context.
+
+            Set the source text and page number from the context for each question.
+            """
+
+            #call LLM
+            raw_response = self.model.generate_content(prompt)
+
+            #return json exam
             return AssessmentSchema.model_validate_json(raw_response)
+
         except Exception as e:
             # Handle retry logic or fallback if JSON is invalid
             print(f"Schema Validation Failed: {e}")
-
-    def call_llm(prompt):
-        #code to make the actual api call here
-        print(f"api_call {prompt}")
